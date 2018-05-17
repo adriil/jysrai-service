6a79cbe837013fed443d0f324f06f06bb8b072d9
Adrien Lesur <adrienl.dev@gmail.com>
2018-05-17 23:48:14 +0200

    Initial commit

---
+++ src/main/java/fr/adriil/jysrai/jysraiservice/JysraiServiceApplication.java
@@ -0,0 +1,12 @@
+package fr.adriil.jysrai.jysraiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JysraiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JysraiServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/adriil/jysrai/jysraiservice/JysraiServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.adriil.jysrai.jysraiservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JysraiServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
